877507609c6b7ce14ec1c2c89cfa5090e81ce2f1
unknown <bleachevil@gmail.com>
2022-04-14 09:24:06 -0400

    update

--- Ratio.py
+++ Ratio.py
@@ -4,6 +4,7 @@ import datetime as dt
 import os
 import matplotlib.pyplot as plt
 import pytz
+from random import sample
 # import hvplot.pandas 
 
 def sharpe_rate(df):
@@ -206,3 +207,48 @@ class MCSimulation:
         ci_series = self.confidence_interval
         ci_series.index = ["95% CI Lower","95% CI Upper"]
         return metrics.append(ci_series)
+
+def plot_MC(df,name, num_days): 
+    #df = get_data(ticker, start_date, end_date)
+    initial_price = 200 #df.iloc[-1].values[0]
+
+    pct_changes = df.pct_change()
+    pct_changes.dropna(inplace=True)
+    pct_changes.index = pd.to_datetime(pct_changes.index)
+
+    possible_changes = list(pct_changes.values)
+
+
+    plt.figure(figsize=(20,10))
+
+    realizations = []
+    for i in range(0, 500): 
+
+        current_price = initial_price 
+        current_price_history = []
+        for i in range(num_days): 
+            current_pct_change = sample(possible_changes,1)[0]
+            current_price = current_price * (1+ current_pct_change)
+            current_price_history.append(current_price)
+
+
+        plt.plot(current_price_history)
+        plt.title(f"Monte Carlo Simulation for {name}")
+
+        realizations.append(current_price_history)
+
+    plt.show()
+
+    closing_prices_as_of_last_day = [x[-1] for x in realizations]
+    expected_return = (np.mean(closing_prices_as_of_last_day) - initial_price)/initial_price
+    low_bound =  np.mean(closing_prices_as_of_last_day)  - 2*np.std(closing_prices_as_of_last_day)
+    upper_bound =  np.mean(closing_prices_as_of_last_day)  + 2*np.std(closing_prices_as_of_last_day)
+
+    print(f"The starting closing price at the end of this period was {initial_price}")
+    print(f"The expected return on {name} is {expected_return} and the closing price will be between {low_bound} and {upper_bound} with 95% confidence in the next {num_days} trading days")
+
+    #low_bound =  np.mean(closing_prices_as_of_last_day)  - 1*np.std(closing_prices_as_of_last_day)
+    #upper_bound =  np.mean(closing_prices_as_of_last_day)  + 1*np.std(closing_prices_as_of_last_day)
+
+    #print(f"The starting closing price at the end of this period was {initial_price}")
+    #print(f"The expected return on {name} is {expected_return} and the closing price will be between {low_bound} and {upper_bound} with 67% confidence in the next {num_days} trading days")
